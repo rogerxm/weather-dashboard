c339388badcf173512e20a03acb3bc45184b3e3d
Roger Montes <rogerxmontes@gmail.com>
2026-05-04 22:42:42 -0600

    feat: add SearchInput component for city search functionality

---
+++ src/components/SearchInput.vue
@@ -0,0 +1,11 @@
+<template>
+  <div class="bg-gray-100 rounded-2xl shadow px-6 py-2">
+    <input class="w-full bg-transparent border-none focus:outline-none" placeholder="Search city..." />
+  </div>
+</template>
+
+<script setup lang="ts">
+
+</script>
+
+<style scoped></style>
