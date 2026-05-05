7664c3a048b1e325e3234361eeecaea294788653
Roger Montes <rogerxmontes@gmail.com>
2026-05-04 22:43:02 -0600

    feat: enhance HomeView layout with CardContainer and loading state

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,10 +1,29 @@
 <template>
-  <div>
+  <div class="min-h-screen flex items-center justify-center">
+    <CardContainer>
+      <LoadingSpinner v-if="loading" />
 
+      <div v-else>
+        <SearchInput />
+      </div>
+    </CardContainer>
   </div>
 </template>
 
 <script setup lang="ts">
+import CardContainer from '@/components/CardContainer.vue';
+import LoadingSpinner from '@/components/LoadingSpinner.vue';
+import SearchInput from '@/components/SearchInput.vue';
+import { useWeather } from '@/composables/useWeather';
+
+const defaultCity = {
+  lat: 19.4326,
+  lon: -99.1332,
+}
+
+const { weather, loading, error } = useWeather(defaultCity.lat, defaultCity.lon);
+
+
 
 </script>
 
